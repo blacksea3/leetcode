55e349e9f3a502511d1935146b6f4ea82f2946fc
XianTao Jia <17865191779@163.com>
2019-06-12 21:49:07 +0800

    reupdate leetcode problem 542 , use BFS, need further optimizing :(

--- cpp/leetcode/542. 01-matrix.cpp
+++ cpp/leetcode/542. 01-matrix.cpp
@@ -1,38 +1,58 @@
 #include "public.h"
 
-//BFS problem
-//I just copy another solution, I will write my own code later
+//BFS problem, 468ms, 39.86%
 
 class Solution {
 public:
 	vector<vector<int>> updateMatrix(vector<vector<int>>& matrix) {
-		queue <pair<int, int>> q;
+		queue <pair<int, int>> q;    //pre level points
 		int m = matrix.size();
 		int n = matrix[0].size();
-		vector <pair<int, int>> around = { {0,1},{0,-1},{-1,0},{1,0} }; //周围节点
+		vector<vector<int>> res(m, vector<int>(n, INT_MAX));
 		for (int i = 0; i < m; ++i)
 		{
 			for (int j = 0; j < n; ++j)
 			{
-				if (matrix[i][j] == 0) q.push({ i,j }); //将元素为0 的点推入队列
+				if (matrix[i][j] == 0)
+				{
+					res[i][j] = 0;
+					q.push({ i,j });    //init q
+				}
 				else matrix[i][j] = INT_MAX;
 			}
 		}
 		while (!q.empty())
 		{
 			pair <int, int> temp = q.front();
+			int prerow = temp.first;
+			int precolumn = temp.second;
+			int preval_p1 = matrix[prerow][precolumn] + 1;
 			q.pop();
-			for (int b = 0; b < 4; ++b) //探索周围节点
-			{
-				int y = temp.first + around[b].first;
-				int x = temp.second + around[b].second;
-				//判断在图内，且新点的元素大于该点元素。
-				if (0 <= x && x < n && 0 <= y && y < m && matrix[temp.first][temp.second] < matrix[y][x])
+			//search neighbour points
+			if (prerow > 0)
+				if (preval_p1 < matrix[prerow - 1][precolumn])
 				{
-					matrix[y][x] = matrix[temp.first][temp.second] + 1;
-					q.push({ y,x });
+					matrix[prerow - 1][precolumn] = preval_p1;
+					q.push({ prerow - 1, precolumn });
 				}
-			}
+			if (prerow < m - 1)
+				if (preval_p1 < matrix[prerow + 1][precolumn])
+				{
+					matrix[prerow + 1][precolumn] = preval_p1;
+					q.push({ prerow + 1, precolumn });
+				}
+			if (precolumn > 0)
+				if (preval_p1 < matrix[prerow][precolumn - 1])
+				{
+					matrix[prerow][precolumn - 1] = preval_p1;
+					q.push({ prerow, precolumn - 1 });
+				}
+			if (precolumn < n - 1)
+				if (preval_p1 < matrix[prerow][precolumn + 1])
+				{
+					matrix[prerow][precolumn + 1] = preval_p1;
+					q.push({ prerow, precolumn + 1 });
+				}
 		}
 		return matrix;
 	}
